ff9ef12bce8f3727b8757a54f478d0637e5ac4d6
Sergey Lepesev <lepesevss@yandex.ru>
2018-03-18 18:10:59 +0500

    add facade pattern and start initialization in main

---
+++ Facade/Facade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Facade.Game;
+using Char = Facade.Game.Char;
+
+namespace Facade
+{
+    public class Facade
+    {
+        private readonly GameLocation _location;
+        private readonly IEnumerable<Char> _chars;
+        private readonly DifficultyGame _difficulty;
+
+        public Facade(GameLocation location, IEnumerable<Char> chars, DifficultyGame difficulty)
+        {
+            _location = location;
+            _chars = chars;
+            _difficulty = difficulty;
+        }
+
+        public Task InitLocation()
+        {
+            return Task.Run(() => _location.Init(_difficulty));
+        }
+
+        public Task InitChars()
+        {
+            return Task.Run(() =>
+            {
+                foreach (var item in _chars)
+                {
+                    item.Init();
+                }
+            });
+        }
+
+    }
+}
--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Facade.Game;
+using Facade.Game.Character;
+using Facade.Game.Enemy;
+using Facade.Game.Location;
+using Char = Facade.Game.Char;
 
 namespace Facade
 {
@@ -6,7 +14,24 @@ namespace Facade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitGame();
+            //initializing another
+            Console.ReadKey();
+        }
+
+        public static void InitGame()
+        {
+            var facade = new Facade(
+                new Wasteland(),
+                new List<Char>
+                {
+                    new SuperMax(),
+                    new WildMan()
+                },
+                DifficultyGame.Low
+                );
+            facade.InitChars();
+            facade.InitLocation();
         }
     }
 }
