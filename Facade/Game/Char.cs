2b4d43ff240c75b5ff1b0c46903a55d84fdcdb2e
Sergey Lepesev <lepesevss@yandex.ru>
2018-03-18 17:29:02 +0500

    add base class for init chars and location

---
+++ Facade/Game/Char.cs
@@ -0,0 +1,13 @@
+﻿namespace Facade.Game
+{
+    public abstract class Char
+    {
+        public abstract string Name { get; }
+        protected SettingsDamage _settingsDamage;
+        public int Health => _settingsDamage.Health;
+        public int Mana => _settingsDamage.Mana;
+        public int Damage => _settingsDamage.Damage;
+
+        public abstract void Init();
+    }
+}
---
+++ Facade/Game/Location.cs
@@ -0,0 +1,10 @@
+﻿namespace Facade.Game
+{
+    public abstract class Location
+    {
+        public abstract string Name { get; }
+        public abstract int CountEnemies { get; }
+
+        public abstract void Init(DifficultyGame difficulty);
+    }
+}
